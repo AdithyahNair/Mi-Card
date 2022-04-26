33d9a6fe8a19b4cf1c785da482a018667ef04413
Adithyah Nair <adithyah.nair@gmail.com>
2022-04-26 07:12:05 +0530

    Initial Commit

---
+++ Mi Card/Mi_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mi_CardApp.swift
+//  Mi Card
+//
+//  Created by Adithyah Nair on 26/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Mi_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
